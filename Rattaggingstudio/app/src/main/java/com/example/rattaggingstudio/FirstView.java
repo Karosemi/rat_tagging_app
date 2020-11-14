8f5e12e4b6da926a19ea3d9c9e7374f9444b6037
Karosemi <karsch6@wp.pl>
2020-11-14 15:51:48 +0100

    with not visible first view

---
+++ Rattaggingstudio/app/src/main/java/com/example/rattaggingstudio/FirstView.java
@@ -0,0 +1,39 @@
+package com.example.rattaggingstudio;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class FirstView extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.first_view, container, false);
+    }
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.addTitle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+        view.findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstView.this)
+                        .navigate(R.id.action_firstView_to_secondView);
+            }
+        });
+    }
+
+}
---
+++ Rattaggingstudio/app/src/main/java/com/example/rattaggingstudio/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.rattaggingstudio;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
