6cb37d7d2ea80d1a1245e145a750eaab536a84e9
Karosemi <karsch6@wp.pl>
2020-11-24 17:08:24 +0100

    add current date

--- Rattaggingstudio/app/src/main/java/com/example/rattaggingstudio/FirstView.java
+++ Rattaggingstudio/app/src/main/java/com/example/rattaggingstudio/FirstView.java
@@ -9,6 +9,9 @@ import android.widget.EditText;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
 
 import static com.example.rattaggingstudio.MainActivity.sqlDataHelper;
 
@@ -23,18 +26,18 @@ public class FirstView extends Fragment {
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-
-
         super.onViewCreated(view, savedInstanceState);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDateTime now = LocalDateTime.now();
         EditText addTitle = (EditText) view.findViewById(R.id.addTitle);
         EditText editDate = (EditText) view.findViewById(R.id.editDate);
+        editDate.setText(dtf.format(now));
         Button next = (Button) view.findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
                                     @Override
                                     public void onClick(View view) {
                                         String titleString = (String) addTitle.getText().toString();
                                         String dateString = (String) editDate.getText().toString();
-                                        String info = titleString + ',' + dateString;
                                         sqlDataHelper.addDataToInfoTable(titleString, dateString);
                                         view.clearFocus();
                                         NavHostFragment.findNavController(FirstView.this)
